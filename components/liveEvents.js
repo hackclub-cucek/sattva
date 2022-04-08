6d2c2f17c873ce83ec5b323404d2b3d22c99dd93
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2022-04-08 13:16:14 +0530

    Live Events

--- components/liveEvents.js
+++ components/liveEvents.js
@@ -10,23 +10,33 @@ export default function LiveEvents() {
             <div className='flex lg:flex-row flex-col w-full lg:max-w-3xl lg:space-x-4 space-y-2 lg:space-y-0'>
                 <div className='w-full max-w-3xl lg:space-y-4 space-y-2'>
                     <div className='flex flex-col space-y-2 bg-grayshade lg:px-12 lg:py-12 px-8 py-8 rounded-lg'>
-                        <h2 className='font-roboto font-semibold text-xl '>Short Story Writing English</h2>
+                        <h2 className='font-roboto font-semibold text-xl '>Water Coloring</h2>
                         <p className='font-roboto font-normal text-sm lg:text-lg'>10:00 AM</p>
                     </div>
                     <div className='flex flex-col space-y-2 bg-grayshade lg:px-12 lg:py-12 px-8 py-8 rounded-lg'>
-                        <h2 className='font-roboto font-semibold text-xl '>Short Story Writing Malayalam</h2>
-                        <p className='font-roboto font-normal text-sm lg:text-lg'>11:00 AM</p>
+                        <h2 className='font-roboto font-semibold text-xl '>Quiz</h2>
+                        <p className='font-roboto font-normal text-sm lg:text-lg'>10:30 AM</p>
                     </div>
                 </div>
                 <div className='w-full max-w-3xl lg:space-y-4 space-y-2'>
                     <div className='flex flex-col space-y-2 bg-grayshade lg:px-12 lg:py-12 px-8 py-8 rounded-lg'>
-                        <h2 className='font-roboto font-semibold text-xl '>Short Story Writing Hindi</h2>
-                        <p className='font-roboto font-normal text-sm lg:text-lg'>11:00 AM</p>
-                    </div>
-                    <div className='flex flex-col space-y-2 bg-grayshade lg:px-12 lg:py-12 px-8 py-8 rounded-lg'>
-                        <h2 className='font-roboto font-semibold text-xl '>Pencil Drawing</h2>
+                        <h2 className='font-roboto font-semibold text-xl '>Cartooning</h2>
                         <p className='font-roboto font-normal text-sm lg:text-lg'>02:00 PM</p>
                     </div>
+                    <div className='flex flex-col space-y-2 bg-grayshade lg:px-12 lg:py-12 px-8 py-8 rounded-lg'>
+                        <h2 className='font-roboto font-semibold text-xl '>Oil Coloring</h2>
+                        <p className='font-roboto font-normal text-sm lg:text-lg'>02:00 PM</p>
+                    </div>
+                </div>
+                <div className='w-full max-w-3xl lg:space-y-4 space-y-2'>
+                    <div className='flex flex-col space-y-2 bg-grayshade lg:px-12 lg:py-12 px-8 py-8 rounded-lg'>
+                        <h2 className='font-roboto font-semibold text-xl '>Film Review English</h2>
+                        <p className='font-roboto font-normal text-sm lg:text-lg'>11:00 AM</p>
+                    </div>
+                    {/* <div className='flex flex-col space-y-2 bg-grayshade lg:px-12 lg:py-12 px-8 py-8 rounded-lg'>
+                        <h2 className='font-roboto font-semibold text-xl '>Pencil Drawing</h2>
+                        <p className='font-roboto font-normal text-sm lg:text-lg'>02:00 PM</p>
+                    </div> */}
                 </div>
             </div>
         </div>
