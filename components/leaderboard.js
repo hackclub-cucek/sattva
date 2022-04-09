91fd71ca3ccde84c0775bd89cd9d3bb1f7361d53
Suvarnesh K M <51795492+SuvarneshKM@users.noreply.github.com>
2022-04-10 02:26:16 +0530

    delay

--- components/leaderboard.js
+++ components/leaderboard.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-export default function Leaderboard({data}) {
-    console.log(data)
-    return (
-        <div className='flex flex-col lg:flex-row justify-between bg-black text-white lg:pt-20 pt-32 lg:px-24 px-4'>
-            <div className='flex flex-col lg:space-y-2 pb-6 lg:pb-0'>
-                <h2 className='font-roboto font-bold lg:text-4xl text-2xl'>Leaderboard</h2>
-                <p className='flex font-roboto font-normal lg:text-xl text-base'>Total Points</p>
-            </div>
-            <div className='w-full max-w-3xl space-y-[10px] font-roboto font-semibold lg:text-xl text-base'>
-                <div className='flex justify-between bg-white bg-opacity-10 px-5 py-4 rounded-lg'>
-                    <div className='flex justify-start space-x-6'>
-                        <p className='text-left'>No.</p>
-                        <p className='text-left'>Department</p>
-                    </div>
-                    <p className='text-right'>Points</p>
-                </div>
-                {data.map((item, index) => (
-                    <div key={index} className="flex justify-between px-5 py-4 rounded-lg border-2 border-white border-opacity-50 ">
-                        <div className='flex justify-start space-x-10'>
-                            <p className='text-left'>{index + 1}</p>
-                            <p className='text-left'>{item[0]}</p>
-                        </div>
-                        <p className='text-right'>{item[1]}</p>
-                    </div>
-                ))}
-            </div>
+export default function Leaderboard({ data }) {
+  console.log(data, 'Working..');
+  return (
+    <div className='flex flex-col lg:flex-row justify-between bg-black text-white lg:pt-20 pt-32 lg:px-24 px-4'>
+      <div className='flex flex-col lg:space-y-2 pb-6 lg:pb-0'>
+        <h2 className='font-roboto font-bold lg:text-4xl text-2xl'>
+          Leaderboard
+        </h2>
+        <p className='flex font-roboto font-normal lg:text-xl text-base'>
+          Total Points
+        </p>
+      </div>
+      <div className='w-full max-w-3xl space-y-[10px] font-roboto font-semibold lg:text-xl text-base'>
+        <div className='flex justify-between bg-white bg-opacity-10 px-5 py-4 rounded-lg'>
+          <div className='flex justify-start space-x-6'>
+            <p className='text-left'>No.</p>
+            <p className='text-left'>Department</p>
+          </div>
+          <p className='text-right'>Points</p>
         </div>
-    )
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className='flex justify-between px-5 py-4 rounded-lg border-2 border-white border-opacity-50 '
+          >
+            <div className='flex justify-start space-x-10'>
+              <p className='text-left'>{index + 1}</p>
+              <p className='text-left'>{item[0]}</p>
+            </div>
+            <p className='text-right'>{item[1]}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,35 @@
 /* eslint-disable @next/next/no-page-custom-font */
-import Head from 'next/head'
-import OffStageEvents from '../components/offStageEvents'
-import FeeDetails from '../components/feeDetails'
-import Banner from '../components/banner'
-import Footer from '../components/footer'
-import Leaderboard from '../components/leaderboard'
-import LiveEvents from '../components/liveEvents'
+import Head from 'next/head';
+import OffStageEvents from '../components/offStageEvents';
+import FeeDetails from '../components/feeDetails';
+import Banner from '../components/banner';
+import Footer from '../components/footer';
+import Leaderboard from '../components/leaderboard';
+import LiveEvents from '../components/liveEvents';
 import { google } from 'googleapis';
 
-export default function Home({rows}) {
+export default function Home({ rows }) {
   return (
-    <div className="">
+    <div className=''>
       <Head>
         <title>SATTVA&apos;22</title>
-        <meta name="description" content="Sattva Website" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Sattva Website' />
+        <link rel='icon' href='/favicon.ico' />
         <link
           href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
           rel='stylesheet'
         />
-        <link href="https://fonts.googleapis.com/css2?family=Plaster&display=swap"
-          rel="stylesheet"
+        <link
+          href='https://fonts.googleapis.com/css2?family=Plaster&display=swap'
+          rel='stylesheet'
         />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
+        <link
+          href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
+          rel='stylesheet'
         />
       </Head>
 
-      <main className="">
+      <main className=''>
         <Banner />
         <Leaderboard data={rows} />
         {/* <LiveEvents /> */}
@@ -36,25 +38,26 @@ export default function Home({rows}) {
         <Footer />
       </main>
     </div>
-  )
-  }
-
-  export async function getStaticProps() {
-    const auth = await google.auth.getClient({scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']});
-    const sheets = google.sheets({ version: 'v4', auth});
-
-    const range = `data!A2:B8`
-    const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: process.env.SHEET_ID,
-      range
-    })
-
-    const rows = response.data.values;
-    return {
-      props: {
-        rows,
-      },
-    }
-    
+  );
 }
 
+export async function getStaticProps() {
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  });
+  const sheets = google.sheets({ version: 'v4', auth });
+
+  const range = `data!A2:B8`;
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range,
+  });
+
+  const rows = response.data.values;
+  return {
+    props: {
+      rows,
+    },
+    revalidate: 10,
+  };
+}
