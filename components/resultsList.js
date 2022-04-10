fccda43bcef911aed93c2f0a26ee3b69849b01ca
suvarneshkm <suvarnesh1729@gmail.com>
2022-04-10 23:08:18 +0530

    fix

--- components/resultsList.js
+++ components/resultsList.js
@@ -4,7 +4,7 @@ import ArrowRightSLineIcon from 'remixicon-react/ArrowRightSLineIcon';
 
 export default function ResultsList({ data }) {
     return (
-        <div className='flex flex-wrap gap-10 bg-black text-white lg:py-36 py-20 lg:px-24 px-4 '>
+        <div className='flex flex-wrap gap-5 bg-black text-white lg:py-36 py-20 lg:px-24 px-4 '>
             {data.map((item, index) => (
                 <div key={index} className='flex flex-col w-full lg:max-w-sm'>
                     <Disclosure>
@@ -18,51 +18,57 @@ export default function ResultsList({ data }) {
                                     />
                                 </Disclosure.Button>
                                 <Disclosure.Panel className="lg:px-5 lg:py-6 px-4 py-[18px] my-2 rounded-lg border border-light border-opacity-50">
-                                    <div className='flex flex-col space-y-4'>
-                                        <h1 className='text-lg lg:text-xl font-semibold'>First Prize</h1>
+                                    <div className='flex flex-col space-y-6'>
                                         <div className='flex flex-col space-y-2'>
-                                            {
-                                                item.first.split(',').map((item, index) => (
-                                                    <div key={index} className='flex justify-between'>
-                                                        {
-                                                            item.split('-').map((id, index) => (
-                                                                <p key={index} className='text-base lg:text-lg font-normal'>
-                                                                    {id}
-                                                                </p>
-                                                            ))}
-                                                    </div>
-                                                ))
-                                            }
+                                            <h1 className='text-lg lg:text-xl font-semibold'>First Prize</h1>
+                                            <div className='flex flex-col space-y-2'>
+                                                {
+                                                    item.first.split(',').map((item, index) => (
+                                                        <div key={index} className='flex justify-between'>
+                                                            {
+                                                                item.split('-').map((id, index) => (
+                                                                    <p key={index} className='text-base lg:text-lg font-normal'>
+                                                                        {id}
+                                                                    </p>
+                                                                ))}
+                                                        </div>
+                                                    ))
+                                                }
+                                            </div>
                                         </div>
-                                        <h1 className='text-lg lg:text-xl font-semibold'>Second Prize</h1>
                                         <div className='flex flex-col space-y-2'>
-                                            {
-                                                item.second.split(',').map((item, index) => (
-                                                    <div key={index} className='flex justify-between'>
-                                                        {
-                                                            item.split('-').map((id, index) => (
-                                                                <p key={index} className='text-base lg:text-lg font-normal'>
-                                                                    {id}
-                                                                </p>
-                                                            ))}
-                                                    </div>
-                                                ))
-                                            }
+                                            <h1 className='text-lg lg:text-xl font-semibold'>Second Prize</h1>
+                                            <div className='flex flex-col space-y-2'>
+                                                {
+                                                    item.second.split(',').map((item, index) => (
+                                                        <div key={index} className='flex justify-between'>
+                                                            {
+                                                                item.split('-').map((id, index) => (
+                                                                    <p key={index} className='text-base lg:text-lg font-normal'>
+                                                                        {id}
+                                                                    </p>
+                                                                ))}
+                                                        </div>
+                                                    ))
+                                                }
+                                            </div>
                                         </div>
-                                        <h1 className='text-lg lg:text-xl font-semibold'>Third Prize</h1>
                                         <div className='flex flex-col space-y-2'>
-                                            {
-                                                item.third.split(',').map((item, index) => (
-                                                    <div key={index} className='flex justify-between'>
-                                                        {
-                                                            item.split('-').map((id, index) => (
-                                                                <p key={index} className='text-base lg:text-lg font-normal'>
-                                                                    {id}
-                                                                </p>
-                                                            ))}
-                                                    </div>
-                                                ))
-                                            }
+                                            <h1 className='text-lg lg:text-xl font-semibold'>Third Prize</h1>
+                                            <div className='flex flex-col space-y-2'>
+                                                {
+                                                    item.third.split(',').map((item, index) => (
+                                                        <div key={index} className='flex justify-between'>
+                                                            {
+                                                                item.split('-').map((id, index) => (
+                                                                    <p key={index} className='text-base lg:text-lg font-normal'>
+                                                                        {id}
+                                                                    </p>
+                                                                ))}
+                                                        </div>
+                                                    ))
+                                                }
+                                            </div>
                                         </div>
                                     </div>
                                 </Disclosure.Panel>
