8275578a3047538f57ecdf30f0481e3b74ba9a87
superRaptor911 <raptor.inc2018@gmail.com>
2023-01-19 00:35:32 +0530

    Data fix

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col lg:items-start lg:text-left text-center items-center lg:space-y-2 space-y-6 bg-black text-white lg:pt-36 pt-20 lg:px-24 px-4 lg:pb-16 pb-10">
       <h1 className="lg:w-96 font-roboto font-bold lg:text-4xl text-2xl">
-        Follow us on Instagram
+        Follow us on Instagram!
       </h1>
       <a
         href="https://instagram.com/sattva_2022?utm_medium=copy_link"
