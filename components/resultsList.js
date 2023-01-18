ce88a0057c67eadf30a1f61805c4ab6a177295fa
superRaptor911 <raptor.inc2018@gmail.com>
2023-01-18 23:59:14 +0530

    sattva 23 media

--- components/resultsList.js
+++ components/resultsList.js
@@ -1,82 +1,89 @@
-import React from 'react'
-import { Disclosure } from '@headlessui/react'
-import ArrowRightSLineIcon from 'remixicon-react/ArrowRightSLineIcon';
+import React from "react";
+import { Disclosure } from "@headlessui/react";
+import ArrowRightSLineIcon from "remixicon-react/ArrowRightSLineIcon";
 
 export default function ResultsList({ data }) {
-    return (
-        <div className='flex flex-wrap gap-5 bg-black text-white lg:py-36 py-20 lg:px-24 px-4 '>
-            {data.map((item, index) => (
-                <div key={index} className='flex flex-col w-full lg:max-w-sm'>
-                    <Disclosure>
-                        {({ open }) => (
-                            <>
-                                <Disclosure.Button className="flex justify-between items-center text-left lg:px-5 px-4 py-[18px] bg-white bg-opacity-10 rounded-lg">
-                                    <p className='text-xl lg:text-2xl font-semibold'>{item.events}</p>
-                                    <ArrowRightSLineIcon
-                                        className={`${open ? 'transform rotate-90' : ''
-                                            } w-5 h-5`}
-                                    />
-                                </Disclosure.Button>
-                                <Disclosure.Panel className="lg:px-5 lg:py-6 px-4 py-[18px] my-2 rounded-lg border border-light border-opacity-50">
-                                    <div className='flex flex-col space-y-6'>
-                                        <div className='flex flex-col space-y-2'>
-                                            <h1 className='text-lg lg:text-xl font-semibold'>First Prize</h1>
-                                            <div className='flex flex-col space-y-2'>
-                                                {
-                                                    item.first.split(',').map((item, index) => (
-                                                        <div key={index} className='flex justify-between'>
-                                                            {
-                                                                item.split('-').map((id, index) => (
-                                                                    <p key={index} className='text-base lg:text-lg font-normal'>
-                                                                        {id}
-                                                                    </p>
-                                                                ))}
-                                                        </div>
-                                                    ))
-                                                }
-                                            </div>
-                                        </div>
-                                        <div className='flex flex-col space-y-2'>
-                                            <h1 className='text-lg lg:text-xl font-semibold'>Second Prize</h1>
-                                            <div className='flex flex-col space-y-2'>
-                                                {
-                                                    item.second.split(',').map((item, index) => (
-                                                        <div key={index} className='flex justify-between'>
-                                                            {
-                                                                item.split('-').map((id, index) => (
-                                                                    <p key={index} className='text-base lg:text-lg font-normal'>
-                                                                        {id}
-                                                                    </p>
-                                                                ))}
-                                                        </div>
-                                                    ))
-                                                }
-                                            </div>
-                                        </div>
-                                        <div className='flex flex-col space-y-2'>
-                                            <h1 className='text-lg lg:text-xl font-semibold'>Third Prize</h1>
-                                            <div className='flex flex-col space-y-2'>
-                                                {
-                                                    item.third.split(',').map((item, index) => (
-                                                        <div key={index} className='flex justify-between'>
-                                                            {
-                                                                item.split('-').map((id, index) => (
-                                                                    <p key={index} className='text-base lg:text-lg font-normal'>
-                                                                        {id}
-                                                                    </p>
-                                                                ))}
-                                                        </div>
-                                                    ))
-                                                }
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Disclosure.Panel>
-                            </>
-                        )}
-                    </Disclosure>
-                </div>
-            ))}
+  return (
+    <div className="flex flex-wrap gap-5 bg-black text-white lg:py-36 py-20 lg:px-24 px-4 ">
+      {data.map((item, index) => (
+        <div key={index} className="flex flex-col w-full lg:max-w-sm">
+          <Disclosure>
+            {({ open }) => (
+              <>
+                <Disclosure.Button className="flex justify-between items-center text-left lg:px-5 px-4 py-[18px] bg-white bg-opacity-10 rounded-lg">
+                  <p className="text-xl lg:text-2xl font-semibold">
+                    {item.events}
+                  </p>
+                  <ArrowRightSLineIcon
+                    className={`${open ? "transform rotate-90" : ""} w-5 h-5`}
+                  />
+                </Disclosure.Button>
+                <Disclosure.Panel className="lg:px-5 lg:py-6 px-4 py-[18px] my-2 rounded-lg border border-light border-opacity-50">
+                  <div className="flex flex-col space-y-6">
+                    <div className="flex flex-col space-y-2">
+                      <h1 className="text-lg lg:text-xl font-semibold">
+                        First Prize
+                      </h1>
+                      <div className="flex flex-col space-y-2">
+                        {item.first.split(",").map((item, index) => (
+                          <div key={index} className="flex justify-between">
+                            {item.split("-").map((id, index) => (
+                              <p
+                                key={index}
+                                className="text-base lg:text-lg font-normal"
+                              >
+                                {id}
+                              </p>
+                            ))}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                      <h1 className="text-lg lg:text-xl font-semibold">
+                        Second Prize
+                      </h1>
+                      <div className="flex flex-col space-y-2">
+                        {item.second.split(",").map((item, index) => (
+                          <div key={index} className="flex justify-between">
+                            {item.split("-").map((id, index) => (
+                              <p
+                                key={index}
+                                className="text-base lg:text-lg font-normal"
+                              >
+                                {id}
+                              </p>
+                            ))}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="flex flex-col space-y-2">
+                      <h1 className="text-lg lg:text-xl font-semibold">
+                        Third Prize
+                      </h1>
+                      <div className="flex flex-col space-y-2">
+                        {item.third.split(",").map((item, index) => (
+                          <div key={index} className="flex justify-between">
+                            {item.split("-").map((id, index) => (
+                              <p
+                                key={index}
+                                className="text-base lg:text-lg font-normal"
+                              >
+                                {id}
+                              </p>
+                            ))}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
--- pages/results.js
+++ pages/results.js
@@ -1,42 +1,42 @@
-import Head from 'next/head';
-import React from 'react'
-import ResultBanner from '../components/resultBanner';
-import ResultsList from '../components/resultsList';
-import { getDataFromSheets } from './api/results';
+import Head from "next/head";
+import React from "react";
+import ResultBanner from "../components/resultBanner";
+import ResultsList from "../components/resultsList";
+import { getDataFromSheets } from "./api/results";
 
 export default function Results({ data }) {
-    return (
-        <div>
-            <Head>
-                <title>SATTVA&apos;22 Results</title>
-                <meta name='description' content='Sattva Website' />
-                <link rel='icon' href='/favicon.ico' />
-                <link
-                    href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-                    rel='stylesheet'
-                />
-                <link
-                    href='https://fonts.googleapis.com/css2?family=Plaster&display=swap'
-                    rel='stylesheet'
-                />
-                <link
-                    href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
-                    rel='stylesheet'
-                />
-            </Head>
-            <ResultBanner />
-            <ResultsList data={data} />
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>SATTVA&apos;23 Results</title>
+        <meta name="description" content="Sattva Website" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Plaster&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ResultBanner />
+      <ResultsList data={data} />
+    </div>
+  );
 }
-
 
 export async function getStaticProps(context) {
-    const sheet = await getDataFromSheets();
-    return {
-        props: {
-            data: sheet.slice(1, sheet.length), // remove sheet header
-        },
-        revalidate: 1, // In seconds
-    };
+  const sheet = await getDataFromSheets();
+  return {
+    props: {
+      data: sheet.slice(1, sheet.length), // remove sheet header
+    },
+    revalidate: 1, // In seconds
+  };
 }
+
