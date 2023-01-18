94e95913e70e667f86a80e4ea4e8bc9760226a88
superRaptor911 <raptor.inc2018@gmail.com>
2023-01-18 23:32:39 +0530

    sattva 23

---
+++ .session.vim
@@ -0,0 +1,47 @@
+let SessionLoad = 1
+let s:so_save = &g:so | let s:siso_save = &g:siso | setg so=0 siso=0 | setl so=-1 siso=-1
+let v:this_session=expand("<sfile>:p")
+silent only
+silent tabonly
+cd /mnt/store/temp/sattva
+if expand('%') == '' && !&modified && line('$') <= 1 && getline(1) == ''
+  let s:wipebuf = bufnr('%')
+endif
+let s:shortmess_save = &shortmess
+if &shortmess =~ 'A'
+  set shortmess=aoOA
+else
+  set shortmess=aoO
+endif
+badd +47 pages/index.js
+badd +30 components/banner.js
+badd +11 pages/api/sheets.js
+badd +0 components/footer.js
+argglobal
+%argdel
+edit components/footer.js
+argglobal
+balt pages/index.js
+let s:l = 11 - ((10 * winheight(0) + 18) / 37)
+if s:l < 1 | let s:l = 1 | endif
+keepjumps exe s:l
+normal! zt
+keepjumps 11
+normal! 032|
+tabnext 1
+if exists('s:wipebuf') && len(win_findbuf(s:wipebuf)) == 0 && getbufvar(s:wipebuf, '&buftype') isnot# 'terminal'
+  silent exe 'bwipe ' . s:wipebuf
+endif
+unlet! s:wipebuf
+set winheight=1 winwidth=20
+let &shortmess = s:shortmess_save
+let s:sx = expand("<sfile>:p:r")."x.vim"
+if filereadable(s:sx)
+  exe "source " . fnameescape(s:sx)
+endif
+let &g:so = s:so_save | let &g:siso = s:siso_save
+set hlsearch
+nohlsearch
+doautoall SessionLoadPost
+unlet SessionLoad
+" vim: set ft=vim :
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,48 @@
 /* eslint-disable @next/next/no-page-custom-font */
-import Head from 'next/head';
-import OffStageEvents from '../components/offStageEvents';
-import FeeDetails from '../components/feeDetails';
-import Banner from '../components/banner';
-import Footer from '../components/footer';
-import Leaderboard from '../components/leaderboard';
-import LiveEvents from '../components/liveEvents';
-import { getDataFromSheets } from './api/sheets';
+import Head from "next/head";
+import OffStageEvents from "../components/offStageEvents";
+import FeeDetails from "../components/feeDetails";
+import Banner from "../components/banner";
+import Footer from "../components/footer";
+import Leaderboard from "../components/leaderboard";
+import LiveEvents from "../components/liveEvents";
+import { getDataFromSheets } from "./api/sheets";
+import { useRef } from "react";
 
 export default function Home({ data }) {
+  const leaderboardRef = useRef(null);
+  const feeDetailsRef = useRef(null);
+
   return (
-    <div className=''>
+    <div className="">
       <Head>
-        <title>SATTVA&apos;22</title>
-        <meta name='description' content='Sattva Website' />
-        <link rel='icon' href='/favicon.ico' />
+        <title>SATTVA&apos;23</title>
+        <meta name="description" content="Sattva Website" />
+        <link rel="icon" href="/favicon.ico" />
         <link
-          href='https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Plaster&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Plaster&display=swap"
+          rel="stylesheet"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
         />
       </Head>
 
-      <main className=''>
-        <Banner />
-        <Leaderboard data={data} />
+      <main className="">
+        <Banner refs={[leaderboardRef, feeDetailsRef]} />
+        <div ref={leaderboardRef}>
+          <Leaderboard data={data} />
+        </div>
         {/* <LiveEvents /> */}
-        <FeeDetails />
+
+        <div ref={feeDetailsRef}>
+          <FeeDetails />
+        </div>
         {/* <OffStageEvents /> */}
         <Footer />
       </main>
